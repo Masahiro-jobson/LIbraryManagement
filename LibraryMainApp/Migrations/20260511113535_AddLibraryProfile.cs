d13256f48a57f2f2a253e37d9d0455081b115cdc
Masahiro-jobson <m.togasaki1026@gmail.com>
2026-05-11 21:40:04 +1000

    Db is updated, libraryProfile

---
+++ LibraryMainApp/Migrations/20260511113535_AddLibraryProfile.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryMainApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLibraryProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LibraryProfile",
+                columns: table => new
+                {
+                    LibraryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StaffID = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OperatingHours = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LoanDurationDays = table.Column<int>(type: "int", nullable: false),
+                    MaxBorrowableBooks = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibraryProfile", x => x.LibraryID);
+                    table.ForeignKey(
+                        name: "FK_LibraryProfile_Staffs_StaffID",
+                        column: x => x.StaffID,
+                        principalTable: "Staffs",
+                        principalColumn: "StaffID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibraryProfile_StaffID",
+                table: "LibraryProfile",
+                column: "StaffID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LibraryProfile");
+        }
+    }
+}
